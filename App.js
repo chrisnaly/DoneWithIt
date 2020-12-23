bb52759eea4b2be2887817929af4c847c0ac0c63
Chrisna <60117964+chrisnaly@users.noreply.github.com>
2020-12-23 14:45:01 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableNativeFeedback, View, Image, SafeAreaView, Alert, Button } from 'react-native';
 
 export default function App() {
   console.log("App Executed");
-  const handlePress = () => console.log("Text pressed");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress} > HELLO REACT NATIVE!!! The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. </Text>
+      <Text numberOfLines={4}> HELLO REACT NATIVE!!! </Text>
+      <Button
+        color="orange"
+        title="Question"
+        onPress={() =>
+        Alert.prompt("My title", "My message", (text) => console.log(text))} />
+
+
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
