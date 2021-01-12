3173ba24aebffdc90c0059dcdddeaa6cbba8d373
Chrisna <60117964+chrisnaly@users.noreply.github.com>
2021-01-12 15:42:51 -0800

    Add image screen

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 
 import React from 'react';
 import { View } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   console.log("App Executed");
 
   return (
-    <WelcomeScreen />
+    //<WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
